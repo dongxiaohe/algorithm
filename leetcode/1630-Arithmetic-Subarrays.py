7908cf8205fc22af87f741cde439ab318fc92b17
Xiaohe Dong <xiaohedong3000@fb.com>
2020-11-17 16:20:07 -0800

    refactor to use zip

--- leetcode/1630-Arithmetic-Subarrays.py
+++ leetcode/1630-Arithmetic-Subarrays.py
@@ -1,15 +1,13 @@
 class Solution:
-    def isArithmetic(self, arr):
-        st = set(arr)
-        if len(st) != len(arr): return len(st) == 1
-        mn, mx = min(arr), max(arr)
-        if (mx - mn) % (len(arr) - 1) != 0: return False # make sure float value cast to integer
-        diff = (mx - mn) // (len(arr) - 1) 
-        for step in range(mn, mx, diff):
-            if step not in st: return False
-        return True
     def checkArithmeticSubarrays(self, nums: List[int], l: List[int], r: List[int]) -> List[bool]:
-        result = []
-        for i in range(len(l)):
-            result.append(self.isArithmetic(nums[l[i]: r[i] + 1]))
-        return result
+        def isArithmetic(arr):
+            st = set(arr)
+            if len(st) != len(arr): return len(st) == 1
+            mn, mx = min(arr), max(arr)
+            if (mx - mn) % (len(arr) - 1) != 0: return False # make sure float value cast to integer
+            diff = (mx - mn) // (len(arr) - 1) 
+            for step in range(mn, mx, diff):
+                if step not in st: return False
+            return True
+        return [isArithmetic(nums[start: stop + 1]) for start, stop in zip(l, r)]
+
