762550ff15a394c3b4dec4f470682a486387d858
Xiaohe Dong <xiaohedong3000@fb.com>
2020-11-21 10:48:40 -0800

    Add Solution for 1603. Design Parking System

--- leetcode/1603-Design-Parking-System.py
+++ leetcode/1603-Design-Parking-System.py
@@ -1,13 +1,10 @@
 class ParkingSystem(object):
 
     def __init__(self, big, medium, small):
-        self.spaces = collections.defaultdict(int)
-        self.spaces[1] = big
-        self.spaces[2] = medium
-        self.spaces[3] = small
+        self.spaces = [big, medium, small]
 
     def addCar(self, carType):
-        if self.spaces[carType] >= 1:
-            self.spaces[carType] -= 1
+        if self.spaces[carType - 1] >= 1:
+            self.spaces[carType - 1] -= 1
             return True
         return False
