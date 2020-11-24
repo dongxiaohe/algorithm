f9734cc6c867eca3fbdb1773e74eb025cd40f6d0
Xiaohe Dong <xiaohedong3000@fb.com>
2020-11-24 11:37:50 -0800

    Modify the solution

--- leetcode/1582-Special-Positions-in-a-Binary-Matrix.py
+++ leetcode/1582-Special-Positions-in-a-Binary-Matrix.py
@@ -3,13 +3,13 @@ class Solution:
         rows, cols = collections.defaultdict(int), collections.defaultdict(int)
         for i in range(len(mat)):
             for j in range(len(mat[0])):
-                if matrix[i][j]:
+                if mat[i][j]:
                     rows[i] += 1
-                    cols[i] += 1
+                    cols[j] += 1
         res = 0
         for i in range(len(mat)):
             for j in range(len(mat[0])):
-                if mat[i][j] == 1 and rows[i] == 1 and cols[j] == 1:
+                if mat[i][j] and rows[i] == 1 and cols[j] == 1:
                     res += 1
         return res
             
