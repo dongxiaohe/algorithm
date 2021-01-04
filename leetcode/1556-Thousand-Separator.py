579e8c375e55b8c5c5939b45546ae95f79611eec
Xiaohe Dong <xiaohedong3000@fb.com>
2021-01-04 12:42:49 -0800

    Add Solution for 1556. Thousand Separator

--- leetcode/1556-Thousand-Separator.py
+++ leetcode/1556-Thousand-Separator.py
@@ -1,7 +1,7 @@
 class Solution:
     def thousandSeparator(self, n: int) -> str:
         s = str(n)
-        start = len(s) 
+        start = len(s)
         deque = collections.deque()
         while start >= 1:
             i = 0 if start - 3 < 0 else start - 3
@@ -13,3 +13,11 @@ class Solution:
             if len(deque) > 0:
                 result = "." + result
         return result
+
+    def thousandSeparator(self, n: int) -> str:
+        s = str(n)
+        s = s[::-1]
+        # 1. t = "1234567" t[0:7] == t[0:100]
+        # 2. s[i: i + 3] for i in range(0, len(s), 3) return a generator
+        result = ".".join(s[i:i + 3] for i in range(0, len(s), 3))
+        return result[::-1]
