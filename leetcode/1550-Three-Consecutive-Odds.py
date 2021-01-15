b9507c774eb1769dfb78514316f81d50c1bf4560
Xiaohe Dong <xiaohedong3000@fb.com>
2021-01-14 16:35:57 -0800

    minor refactor

--- leetcode/1550-Three-Consecutive-Odds.py
+++ leetcode/1550-Three-Consecutive-Odds.py
@@ -1,15 +1,9 @@
 class Solution:
     def threeConsecutiveOdds(self, arr: List[int]) -> bool:
-        l, r = 0, 0 
-        while l < len(arr):
-            if arr[l] % 2 == 1:
-                r = l + 1
-                while r < len(arr):
-                    if arr[r] % 2 == 0:
-                        l = r 
-                        break
-                    if r - l == 2:    
-                        return True
-                    r += 1
-            l += 1
+        cnt = 0
+        for val in arr:
+            if val % 2 == 0: cnt = 0
+            else:
+                cnt += 1
+                if cnt == 3: return True
         return False
