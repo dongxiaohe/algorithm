a9032d6cbc930d0c476243090b93c6a9dc9519ef
Xiaohe Dong <dannyrivclo@gmail.com>
2021-03-01 11:29:03 -0800

    Add Solution for 654. Maximum Binary Tree

---
+++ leetcode/654-Maximum-Binary-Tree.py
@@ -0,0 +1,15 @@
+class Solution:
+    def constructMaximumBinaryTree(self, nums: List[int]) -> TreeNode:
+        def _maxAndIndex(arr):
+            _max, position = float("-inf"), -1
+            for i in range(len(arr)):
+                if arr[i] > _max:
+                    _max = arr[i]
+                    position = i
+            return (_max, position)
+        if not nums: return None
+        _max, position = _maxAndIndex(nums)
+        root = TreeNode(_max)
+        root.left = self.constructMaximumBinaryTree(nums[:position])
+        root.right = self.constructMaximumBinaryTree(nums[position + 1:])
+        return root
