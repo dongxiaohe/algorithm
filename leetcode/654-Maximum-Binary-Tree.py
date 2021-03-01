f42bc74e1ad85d67083707ea9ea3d5e12d2be00c
Xiaohe Dong <dannyrivclo@gmail.com>
2021-03-01 11:31:27 -0800

    minor refactor

--- leetcode/654-Maximum-Binary-Tree.py
+++ leetcode/654-Maximum-Binary-Tree.py
@@ -1,14 +1,14 @@
 class Solution:
+    def maxAndIndex(self, arr):
+        _max, position = float("-inf"), -1
+        for i in range(len(arr)):
+            if arr[i] > _max:
+                _max = arr[i]
+                position = i
+        return (_max, position)
     def constructMaximumBinaryTree(self, nums: List[int]) -> TreeNode:
-        def _maxAndIndex(arr):
-            _max, position = float("-inf"), -1
-            for i in range(len(arr)):
-                if arr[i] > _max:
-                    _max = arr[i]
-                    position = i
-            return (_max, position)
         if not nums: return None
-        _max, position = _maxAndIndex(nums)
+        _max, position = self.maxAndIndex(nums)
         root = TreeNode(_max)
         root.left = self.constructMaximumBinaryTree(nums[:position])
         root.right = self.constructMaximumBinaryTree(nums[position + 1:])
