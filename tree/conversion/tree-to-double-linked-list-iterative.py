63acb850771a750e12995f8a6753a08eefa8c1e3
Xiaohe Dong <dannyrivclo@gmail.com>
2021-03-06 11:04:16 -0800

    minor refactor

--- tree/conversion/tree-to-double-linked-list-iterative.py
+++ tree/conversion/tree-to-double-linked-list-iterative.py
@@ -1,4 +1,4 @@
-def treeToList(root):
+def treeToList1(root):
     stack, pre, cur, first = [], None, root, None
     while stack or root:
         while root:
@@ -15,8 +15,27 @@ def treeToList(root):
     cur.right = first
     return first
 
+def treeToList2(root):
+    def _traverseLeft(stack, node):
+        while node:
+            stack.append(node)
+            node = node.left
+    stack, pre, cur, first = [], None, None, None
+    _traverseLeft(stack, root)
+    while stack:
+        cur = stack.pop()
+        if pre is not None:
+            if first is None: first = cur
+            pre.right = cur
+            cur.left = pre
+        pre = cur
+        _traverseLeft(stack, cur.right)
+    first.left = cur
+    cur.right = first
+    return first
+
 from fixture import *
-root = treeToList(three)
+root = treeToList2(three)
 
 i, limit = 0, 6 
 while i < limit:
