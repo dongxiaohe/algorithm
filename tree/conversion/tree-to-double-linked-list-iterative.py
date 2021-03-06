0c39fccac2a8308231ed71bbf3617f1929593c31
Xiaohe Dong <dannyrivclo@gmail.com>
2021-03-06 10:00:09 -0800

    pratice

--- tree/conversion/tree-to-double-linked-list-iterative.py
+++ tree/conversion/tree-to-double-linked-list-iterative.py
@@ -1,48 +1,24 @@
-def traverseLeft(stack, node):
-    while node:
-        stack.append(node)
-        node = node.left
-def treeToList1(root):
-    stack = []
-    first, cur, prev = None, None, None
-    traverseLeft(stack, root)
-    while stack:
-        cur = stack.pop()
-        if prev is not None:
-            if first is None: first = prev
-            cur.left = prev
-            prev.right = cur
-        prev = cur
-        traverseLeft(stack, cur.right)
-    first.left = cur
-    cur.right = first
-    return first
-
 def treeToList(root):
-    if root is None: return root
-    prev, cur, first = None, None, None
-    stack = []
-    while root or stack:
+    stack, pre, cur, first = [], None, root, None
+    while stack or root:
         while root:
             stack.append(root)
             root = root.left
-        cur = stack.pop() 
-        if prev is not None:
-            if not first: first = prev
-            print(prev.val, cur.val)
-            cur.left = prev
-            prev.right = cur
-        prev = cur
+        cur = stack.pop()
+        if pre is not None:
+            if first is None: first = cur
+            pre.right = cur
+            cur.left = pre
+        pre = cur
         root = cur.right
     first.left = cur
     cur.right = first
     return first
 
-
 from fixture import *
-root = treeToList1(three)
+root = treeToList(three)
 
-i, limit = 0, 5 
+i, limit = 0, 6 
 while i < limit:
     print("..................... " + root.val)
     root = root.right
