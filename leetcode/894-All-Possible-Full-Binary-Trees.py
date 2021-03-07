179ad8019957a69076fc6a47ab13878dc8bd37d6
Xiaohe Dong <dannyrivclo@gmail.com>
2021-03-07 11:21:13 -0800

    Add Solution for 894. All Possible Full Binary Trees

--- leetcode/894-All-Possible-Full-Binary-Trees.py
+++ leetcode/894-All-Possible-Full-Binary-Trees.py
@@ -1,13 +1,17 @@
 class Solution:
     def allPossibleFBT(self, n: int) -> List[TreeNode]:
-        n -= 1
-        if n == 0: return [TreeNode(0)]
-        res = []
-        for i in range(1, n, 2):
-            for left in self.allPossibleFBT(i):
-                for right in self.allPossibleFBT(n - i):
-                    root = TreeNode(0)
-                    root.left = left
-                    root.right = right
-                    res.extend([root])
-        return res
+        def _allPossibleFBT(n, mem):
+            if n in mem: return mem[n]
+            n -= 1
+            if n == 0: return [TreeNode(0)]
+            res = []
+            for i in range(1, n, 2):
+                for left in _allPossibleFBT(i, mem):
+                    for right in _allPossibleFBT(n - i, mem):
+                        root = TreeNode(0)
+                        root.left = left
+                        root.right = right
+                        res.extend([root])
+            mem[n] = res
+            return res
+        return _allPossibleFBT(n, {})
