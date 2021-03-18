19a4d2754f935b0c355afe6cd4ad70575cff95bf
Xiaohe Dong <dannyrivclo@gmail.com>
2021-03-18 14:24:37 -0700

    minor structure change

--- leetcode/226-Invert-Binary-Tree.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def invertTree(self, root: TreeNode) -> TreeNode:
-        if root:
-            tmp1 = self.invertTree(root.right)
-            tmp2 = self.invertTree(root.left)
-            root.left = tmp1
-            root.right = tmp2
-            return root
-
