ae9078a63877ce79e8fcadbbeb415822a2995397
Xiaohe Dong <dannyrivclo@gmail.com>
2021-03-20 10:43:52 -0700

    minor comment

--- leetcode/1389-Create-Target-Array-in-the-Given-Order.py
+++ leetcode/1389-Create-Target-Array-in-the-Given-Order.py
@@ -1,7 +1,7 @@
 class Solution:
+    # Note this is a still N ^ 2 solution, there is n * logN solution by using merge sort
     def createTargetArray(self, nums: List[int], index: List[int]) -> List[int]:
         res = []
         for i in range(len(nums)):
             res.insert(index[i], nums[i])
         return res
-    
