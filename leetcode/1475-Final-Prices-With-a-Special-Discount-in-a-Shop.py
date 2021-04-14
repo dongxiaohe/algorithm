c4205bef4f8fc0a9843b37791ca61114c30f9a78
Xiaohe Dong <dannyrivclo@gmail.com>
2021-04-14 12:13:19 -0700

    Change to O(n)

--- leetcode/1475-Final-Prices-With-a-Special-Discount-in-a-Shop.py
+++ leetcode/1475-Final-Prices-With-a-Special-Discount-in-a-Shop.py
@@ -1,13 +1,9 @@
 class Solution:
     def finalPrices(self, prices: List[int]) -> List[int]:
-        res = []
-        for i in range(len(prices)):
-            j = i + 1
-            while j < len(prices):
-                if prices[j] <= prices[i]: 
-                    res.append(prices[i] - prices[j])
-                    break
-                j += 1
-            if len(res) == i:
-                res.append(prices[i])
-        return res
+        stack = []
+        for i, val in enumerate(prices):
+            while stack and prices[stack[-1]] >= val:
+                prices[stack.pop()] -= val
+            stack.append(i)
+        return prices
+
