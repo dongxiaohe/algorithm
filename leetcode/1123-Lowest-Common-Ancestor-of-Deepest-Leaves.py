6ce3e3f55058e4e289ecb70bac5998326197c220
Xiaohe Dong <dannyrivclo@gmail.com>
2021-04-16 10:43:55 -0700

    Add Solution for 1123. Lowest Common Ancestor of Deepest Leaves

---
+++ leetcode/1123-Lowest-Common-Ancestor-of-Deepest-Leaves.py
@@ -0,0 +1,13 @@
+class Solution:
+    def lcaDeepestLeaves(self, root: TreeNode) -> TreeNode:
+        def dfs(node):
+            if not node: return 0, None
+            left_level, left_node = dfs(node.left)
+            right_level, right_node = dfs(node.right)
+            if left_level == right_level:
+                return left_level + 1, node
+            elif left_level > right_level:
+                return left_level + 1, left_node
+            else:
+                return right_level + 1, right_node
+        return dfs(root)[1]
