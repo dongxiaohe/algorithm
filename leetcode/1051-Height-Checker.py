233ab15781af6d6d504cbec403d12b6075e50017
Xiaohe Dong <dannyrivclo@gmail.com>
2021-04-18 12:12:33 -0700

    minor refactor

--- leetcode/1051-Height-Checker.py
+++ leetcode/1051-Height-Checker.py
@@ -1,7 +1,13 @@
 class Solution:
     def heightChecker(self, heights: List[int]) -> int:
-        sortedHeights = sorted(heights)
-        cnt = 0
-        for i in range(len(heights)):
-            if heights[i] != sortedHeights[i]: cnt += 1
+        counter = collections.Counter(heights)
+        cnt, lower, upper, i = 0, 0, 100, 0
+        while lower <= upper:
+            if counter[lower] > 0:
+                if heights[i] != lower: cnt += 1
+                counter[lower] -= 1
+                i += 1
+            if counter[lower] == 0:
+                lower += 1
         return cnt
+        
