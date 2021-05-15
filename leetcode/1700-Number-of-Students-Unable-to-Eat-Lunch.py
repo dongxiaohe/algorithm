fea618190fc412b67d07d3ba38bc4606acae8e0f
Xiaohe Dong <dannyrivclo@gmail.com>
2021-05-15 16:41:28 -0700

    Add Solution for 1700. Number of Students Unable to Eat Lunch

--- leetcode/1700-Number-of-Students-Unable-to-Eat-Lunch.py
+++ leetcode/1700-Number-of-Students-Unable-to-Eat-Lunch.py
@@ -1,13 +1,11 @@
 class Solution:
     def countStudents(self, students: List[int], sandwiches: List[int]) -> int:
-        studentsKv, sandwichesKv = collections.Counter(students), collections.Counter(sandwiches)
-        if studentsKv == sandwichesKv: return 0
-        while students:
-            if studentsKv[sandwiches[0]] == 0: return len(students)
-            head = students.pop(0)
-            if head == sandwiches[0]:
-                sandwiches.pop(0)
-                studentsKv[head] -= 1
+        cnt = collections.Counter(students)
+        k = 0 
+        while k < len(sandwiches):
+            if cnt[sandwiches[k]] > 0:
+                cnt[sandwiches[k]] -= 1
             else:
-                students.append(head)
-        return 0
+                break
+            k += 1
+        return len(sandwiches) - k
