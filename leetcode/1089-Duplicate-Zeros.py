db5349d4b1642a35ab72dbd0f4cb7e0d07f39bee
Xiaohe Dong <dannyrivclo@gmail.com>
2021-05-17 16:14:27 -0700

    Add Solution for 1089. Duplicate Zeros

--- leetcode/1089-Duplicate-Zeros.py
+++ leetcode/1089-Duplicate-Zeros.py
@@ -1,15 +1,10 @@
 class Solution:
     def duplicateZeros(self, arr: List[int]) -> None:
-        i = 0
-        while i < len(arr):
+        zeros = arr.count(0)
+        for i in range(len(arr) - 1, -1, -1):
+            if i + zeros < len(arr):
+                arr[i + zeros] = arr[i]
             if arr[i] == 0:
-                j = len(arr) - 1
-                while j > i + 1:
-                    arr[j] = arr[j - 1]
-                    j -= 1
-                if i < len(arr) - 1:
-                    arr[i + 1] = 0
-                i += 2
-            else:
-                i += 1
-
+                zeros -= 1
+                if i + zeros < len(arr):
+                    arr[i + zeros] = 0
