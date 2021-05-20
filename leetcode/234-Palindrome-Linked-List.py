5770e9ff3f2a52dae3b46c9e40f97dc478aa8131
Xiaohe Dong <dannyrivclo@gmail.com>
2021-05-20 16:58:20 -0700

    Add Solution for 234. Palindrome Linked List

--- leetcode/234-Palindrome-Linked-List.py
+++ leetcode/234-Palindrome-Linked-List.py
@@ -1,25 +1,15 @@
 class Solution:
     def isPalindrome(self, head: ListNode) -> bool:
-        def reverse(node, endNode):
-            prev = None
-            while node != endNode or node != None:
-                tmp = node.next
-                node.next = prev
-                prev = node
-                node = tmp
-            return prev
-        slow, fast = head, head
+        slow, fast, rev = head, head, None
         while fast and fast.next:
-            slow = slow.next
             fast = fast.next.next
+            rev, rev.next, slow = slow, rev, slow.next
         if fast:
             slow = slow.next
-        slow = reverse(slow, None)
-        fast = head
         while slow:
-            if slow.val != fast.val:
+            if slow.val != rev.val:
                 return False
             slow = slow.next
-            fast = fast.next
+            rev = rev.next
         return True
-        
+
