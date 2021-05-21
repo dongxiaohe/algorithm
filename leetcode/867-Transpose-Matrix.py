ccf83777577ae7dc1cabf12fad22f5d8d70915d6
Xiaohe Dong <dannyrivclo@gmail.com>
2021-05-21 15:24:22 -0700

    minor change

--- leetcode/867-Transpose-Matrix.py
+++ leetcode/867-Transpose-Matrix.py
@@ -3,8 +3,5 @@ class Solution:
         res = [[0 for _ in range(len(matrix))] for _ in range(len(matrix[0]))]
         for i in range(len(matrix)):
             for j in range(len(matrix[0])):
-                if i < len(res) and j < len(res[0]):
-                    res[i][j] = matrix[j][i]
-                if j < len(res) and i < len(res[0]):
-                    res[j][i] = matrix[i][j]
+                res[j][i] = matrix[i][j]
         return res
