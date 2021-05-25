b5f20b493de00b975e154c1ce4bd3fae4a9af967
Xiaohe Dong <dannyrivclo@gmail.com>
2021-05-25 11:28:37 -0700

    minor refactor

--- leetcode/1848-Minimum-Distance-to-the-Target-Element.py
+++ leetcode/1848-Minimum-Distance-to-the-Target-Element.py
@@ -3,11 +3,11 @@ class Solution:
         distance, res = float("inf"), -1
         l, r, turn = start, start + 1, True
         while l >= 0 or r < len(nums):
-            if abs(l - start) <= abs(r - start):
-                if l >= 0 and nums[l] == target:
-                    return abs(l - start)
+            if l >= 0 and nums[l] == target:
+                return start - l
                 l -= 1
-            else:
-                if r < len(nums) and nums[r] == target:
-                    return abs(r - start)
-                r += 1
+            if r < len(nums) and nums[r] == target:
+                return r - start
+            r += 1
+            l -= 1
+
