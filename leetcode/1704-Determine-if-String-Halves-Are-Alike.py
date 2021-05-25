629253c562509f504681dd3acbce647eb1c22784
Xiaohe Dong <dannyrivclo@gmail.com>
2021-05-25 12:51:34 -0700

    Minor amend

--- leetcode/1704-Determine-if-String-Halves-Are-Alike.py
+++ leetcode/1704-Determine-if-String-Halves-Are-Alike.py
@@ -1,10 +1,7 @@
 class Solution:
     def halvesAreAlike(self, s: str) -> bool:
-        kv = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U']
-        first, second = 0, 0
+        kv, half, cnt = "aeiouAEIOU", len(s) >> 1, 0
         for i in range(len(s)):
-            if i < (len(s) >> 1):
-                first += 1 if s[i] in kv else 0
-            else:
-                second += 1 if s[i] in kv else 0
-        return first == second
+            if s[i] in kv:
+                cnt += 1 if i < half else -1
+        return  cnt == 0
