a826be3355809dd6764ccf1a999b2a7433573371
Xiaohe Dong <dannyrivclo@gmail.com>
2021-05-28 17:04:25 -0700

    Add Solution for 1381. Design a Stack With Increment Operation

--- leetcode/1381-Design-a-Stack-With-Increment-Operation.py
+++ leetcode/1381-Design-a-Stack-With-Increment-Operation.py
@@ -2,18 +2,22 @@ class CustomStack:
 
     def __init__(self, maxSize: int):
         self.stack = []
+        self.incr = []
         self.maxSize = maxSize
 
     def push(self, x: int) -> None:
         if len(self.stack) < self.maxSize:
             self.stack.append(x)
+            self.incr.append(0)
 
     def pop(self) -> int:
-        if self.stack:
-            return self.stack.pop()
-        return -1
+        if not self.stack: return -1
+        if len(self.stack) > 1:
+            self.incr[-2] += self.incr[-1]
+        return self.stack.pop() + self.incr.pop()
 
     def increment(self, k: int, val: int) -> None:
-        k = min(k, len(self.stack))
-        for i in range(k):
-            self.stack[i] += val
+        if self.incr:
+            k = min(k, len(self.incr)) - 1
+            self.incr[k] += val
+
