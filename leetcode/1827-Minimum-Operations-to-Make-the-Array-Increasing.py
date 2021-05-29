9a49b9ab8a1e59baca4a7f6d8254e95b880fee23
Xiaohe Dong <dannyrivclo@gmail.com>
2021-05-29 14:58:53 -0700

    Minor change

--- leetcode/1827-Minimum-Operations-to-Make-the-Array-Increasing.py
+++ leetcode/1827-Minimum-Operations-to-Make-the-Array-Increasing.py
@@ -1,9 +1,11 @@
 class Solution:
     def minOperations(self, nums: List[int]) -> int:
-        cnt = 0
+        if not nums: return 0
+        cnt, prev = 0, nums[0]
         for i in range(1, len(nums)):
-            if nums[i] <= nums[i - 1]:
-                cnt += nums[i - 1] - nums[i] + 1
-                nums[i] = nums[i - 1] + 1
+            if nums[i] <= prev:
+                cnt += prev - nums[i] + 1
+                prev = prev + 1
+            else:
+                prev = nums[i]
         return cnt
-
