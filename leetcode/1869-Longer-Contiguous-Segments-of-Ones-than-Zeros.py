199ef4727069efd962c5b8ade21bcd1a67f4e94a
Xiaohe Dong <dannyrivclo@gmail.com>
2021-05-30 22:33:29 -0700

    Add Solution for 1869. Longer Contiguous Segments of Ones than Zeros

--- leetcode/1869-Longer-Contiguous-Segments-of-Ones-than-Zeros.py
+++ leetcode/1869-Longer-Contiguous-Segments-of-Ones-than-Zeros.py
@@ -1,19 +1,13 @@
 class Solution:
     def checkZeroOnes(self, s: str) -> bool:
-        zeros, ones, zero, maxZeros, maxOnes = 0, 0, None, 0, 0
+        zeros, ones, maxZeros, maxOnes = 0, 0, 0, 0
         for c in s:
             if c == "0":
-                if zero == True:
-                    zeros += 1
-                else:
-                    zero = True
-                    zeros = 1
+                zeros += 1
                 maxZeros = max(maxZeros, zeros)
+                ones = 0
             else:
-                if zero == False:
-                    ones += 1
-                else:
-                    zero = False
-                    ones = 1
+                ones += 1
                 maxOnes = max(maxOnes, ones)
+                zeros = 0
         return maxOnes > maxZeros
