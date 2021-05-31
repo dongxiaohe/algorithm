eb498aa866bb38aeab1ca52854edb5b912a27bf0
Xiaohe Dong <dannyrivclo@gmail.com>
2021-05-31 15:42:23 -0700

    Add Solution for 1539. Kth Missing Positive Number

--- leetcode/1539-Kth-Missing-Positive-Number.py
+++ leetcode/1539-Kth-Missing-Positive-Number.py
@@ -1,13 +1,10 @@
 class Solution:
     def findKthPositive(self, arr: List[int], k: int) -> int:
-        i, cnt = 1, 1
-        for val in arr: 
-            if i != val:
-                while i < val:
-                    if cnt == k:
-                        return i
-                    i += 1
-                    cnt += 1
-            i += 1
-        return i + k - cnt
-
+        l, r = 0, len(arr)
+        while l < r:
+            m = l + (r - l >> 1)
+            if arr[m] - m - 1 < k:
+                l = m + 1
+            else:
+                r = m
+        return l + k
