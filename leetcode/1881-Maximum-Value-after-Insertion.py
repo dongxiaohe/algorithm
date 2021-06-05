0253de0a9bdb19f9b815b7901d5da62c5b6855fe
Xiaohe Dong <dannyrivclo@gmail.com>
2021-06-05 14:13:49 -0700

    minor change

--- leetcode/1881-Maximum-Value-after-Insertion.py
+++ leetcode/1881-Maximum-Value-after-Insertion.py
@@ -1,13 +1,6 @@
 class Solution:
     def maxValue(self, n: str, x: int) -> str:
-        if not n: return str(x)
-        if n[0] == "-":
-            for i in range(1, len(n)):
-                if int(n[i]) > x:
-                    return n[:i] + str(x) + n[i:]
-            return n + str(x)
-        else:
-            for i in range(len(n)):
-                if int(n[i]) < x:
-                    return n[:i] + str(x) + n[i:]
-            return n + str(x)
+        for i in range(len(n)):
+            if [n[i] < str(x), n[i] > str(x)][n[0] == "-"]:
+                return n[:i] + str(x) + n[i:]
+        return n + str(x)
