83017b26b0d31a242e037c739fd1ee89fa5cd75d
Xiaohe Dong <dannyrivclo@gmail.com>
2021-06-06 15:49:01 -0700

    Add Solution for 1170. Compare Strings by Frequency of the Smallest Character

---
+++ leetcode/1170-Compare-Strings-by-Frequency-of-the-Smallest-Character.py
@@ -0,0 +1,19 @@
+class Solution:
+    def numSmallerByFrequency(self, queries: List[str], words: List[str]) -> List[int]:
+        def getFrequency(word):
+            kv = Counter(word)
+            for i in range(26):
+                ch = chr(i + 97)
+                if kv[ch] > 0: return kv[ch]
+            return -1
+        qCounter = list(map(lambda x: getFrequency(x), queries))
+        wordCounter = list(map(lambda x: getFrequency(x), words))
+        res = []
+        for each in qCounter:
+            cnt = 0
+            for word in wordCounter:
+                if word > each:
+                    cnt += 1
+            res.append(cnt)
+        return res
+
