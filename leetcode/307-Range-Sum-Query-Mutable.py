910557aa0dd72998666cb1d79288fccc60d7f176
Xiaohe Dong <dannyrivclo@gmail.com>
2021-06-13 17:01:19 -0700

    minor change

--- leetcode/307-Range-Sum-Query-Mutable.py
+++ leetcode/307-Range-Sum-Query-Mutable.py
@@ -1,30 +1,30 @@
-class FenwickTree:
-    def __init__(self, nums):
-        self.nums = [0] * (len(nums) + 1)
-        for i in range(len(nums)):
-            self.update(i, nums[i])
-    def update(self, i, delta):
-        i += 1
-        while i < len(self.nums):
-            self.nums[i] += delta
-            i += i & (-i)
-    def query(self, i):
-        i += 1
-        _sum = 0
-        while i > 0:
-            _sum += self.nums[i]
-            i -= i & (-i)
-        return _sum
-            
 class NumArray:
+
     def __init__(self, nums: List[int]):
-        self.fenwickTree = FenwickTree(nums)
         self.nums = nums
+        self.sum = [0 for _ in range(len(nums) + 1)]
+        for i in range(len(nums)):
+            self.updateTree(i, nums[i])
+
+    def updateTree(self, i, delta):
+        i += 1
+        while i < len(self.sum):
+            self.sum[i] += delta
+            i += i & (-i)
 
     def update(self, index: int, val: int) -> None:
         delta = val - self.nums[index]
-        self.fenwickTree.update(index, delta)
-        self.nums[index] += delta
+        self.nums[index] = val
+        self.updateTree(index, delta)
+
+    def sumTree(self, i):
+        total = 0
+        i += 1
+        while i > 0:
+            total += self.sum[i]
+            i -= i & (-i)
+        return total
 
     def sumRange(self, left: int, right: int) -> int:
-        return self.fenwickTree.query(right) - self.fenwickTree.query(left - 1)
+        return self.sumTree(right) - self.sumTree(left - 1)
+
