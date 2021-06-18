b44f13bc1b4b9acd1cabda732b0ea8968295d0d9
Xiaohe Dong <dannyrivclo@gmail.com>
2021-06-18 15:52:08 -0700

    Add Solution for 316. Remove Duplicate Letters

--- leetcode/316-Remove-Duplicate-Letters.py
+++ leetcode/316-Remove-Duplicate-Letters.py
@@ -1,15 +1,15 @@
 class Solution:
-    res = None
     def removeDuplicateLetters(self, s: str) -> str:
-        uniques = set(s)
-        def backtrack(s, uniques, start, acc):
-            if len(acc) > len(uniques): return
-            if set(acc) == uniques:
-                if not self.res: self.res = acc
-                else:
-                    self.res = min(acc, self.res)
-                return
-            for i in range(start, len(s)):
-                backtrack(s, uniques, i + 1, acc + [s[i]])
-        backtrack(s, uniques, 0, [])
-        return "".join(self.res)
+        visited, kv = [False] * 26, defaultdict(int)
+        for ch in s:
+            kv[ch] += 1
+        stack = []
+        for ch in s:
+            kv[ch] -= 1
+            index = ord(ch) - 97
+            if visited[index]: continue
+            while stack and stack[-1] > ch and kv[stack[-1]] > 0:
+                visited[ord(stack.pop()) - 97] = False
+            visited[index] = True
+            stack.append(ch)
+        return "".join(stack)
