e85ca7b32378bc51849dcde056289bd97835ff00
Xiaohe Dong <dannyrivclo@gmail.com>
2021-06-20 16:16:53 -0700

    Add Solution for 224. Basic Calculator

--- leetcode/224-Basic-Calculator.py
+++ leetcode/224-Basic-Calculator.py
@@ -1,21 +1,45 @@
 class Solution:
     def calculate(self, s: str) -> int:
-        op, prev, cur = 1, 0, ""
-        for i in range(len(s)):
-            if s[i] == "+":
-                op = 1
-            elif s[i] == "-":
-                op = -1
-            elif s[i] == "(" or s[i] == ")":
-                continue
-            else:
-                if s[i] != " " and i + 1 < len(s) and not s[i + 1].isdigit():
-                   
-                    prev += int((cur + s[i]).strip() ) * op
-                    cur = ""
+        def eval(s):
+            op, prev, cur = 1, 0, ""
+            for i in range(len(s)):
+                if s[i] == "+":
+                    op = 1
+                elif s[i] == "-":
+                    cnt = 1
+                    j = i - 1
+                    while j >= 0 and s[j] == "-":
+                        cnt += 1
+                        j -= 1
+                    if cnt & 1 == 1:
+                        op = -1
+                    else:
+                        op = 1          
                 else:
-                    cur += s[i]
-        if cur.strip().isdigit():
-            return prev + int(cur.strip()) * op
-        return prev
-    
+                    if s[i] != " " and i + 1 < len(s) and not s[i + 1].isdigit():
+                        prev += int((cur + s[i]).strip() ) * op
+                        cur = ""
+                    else:
+                        cur += s[i]
+            if cur.strip().isdigit():
+                return prev + int(cur.strip()) * op
+            return prev
+        stack, cur, i = [], "", 0
+        while i < len(s) or stack:
+            if (i >= len(s) and stack) or s[i] == ")":
+                tmp = []
+                while stack:
+                    ch = stack.pop()
+                    if ch == "(":
+                        break
+                    tmp.append(ch)
+                tmp = "".join(tmp[::-1]) + cur
+                tmp = eval(tmp)
+                cur = str(tmp)
+            elif s[i] == "(":
+                stack.append(cur)
+                stack.append("(")
+                cur = ""
+            elif s[i] != " ": cur +=s[i]
+            i += 1
+        return eval(cur)
