94a537a15f3eb0d769ffcbf56bed1538da350552
Xiaohe Dong <dannyrivclo@gmail.com>
2021-06-22 15:04:12 -0700

    Refactor the solution to use constant space

--- leetcode/1423-Maximum-Points-You-Can-Obtain-from-Cards.py
+++ leetcode/1423-Maximum-Points-You-Can-Obtain-from-Cards.py
@@ -1,13 +1,11 @@
 class Solution:
     def maxScore(self, cardPoints: List[int], k: int) -> int:
-        if k == len(cardPoints): return sum(cardPoints)
-        arr = cardPoints[len(cardPoints) - k:] + cardPoints[:k]
-        cur = sum(arr[:k])
-        res = cur
-        i = 0 
-        while i < len(arr) - k:
-            cur = cur + arr[k + i] - arr[i]
+        m = len(cardPoints)
+        if k == m: return sum(cardPoints)
+        cur = sum(cardPoints[m - k:])
+        res, i = cur, 0
+        while i < k:
+            cur = cur + cardPoints[i] - cardPoints[m - k + i]
             res = max(res, cur)
             i += 1
         return res
-    
