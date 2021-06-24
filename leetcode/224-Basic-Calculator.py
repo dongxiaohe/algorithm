212063973d6ec356be54bcf5bca79819adb753ba
Xiaohe Dong <dannyrivclo@gmail.com>
2021-06-24 11:35:11 -0700

    Refactor solution to take previous result and sign into the stack rather than prev result and paranthesis in the stack

--- leetcode/224-Basic-Calculator.py
+++ leetcode/224-Basic-Calculator.py
@@ -1,43 +1,27 @@
 class Solution:
     def calculate(self, s: str) -> int:
-        def eval(s):
-            op, prev, cur = 1, 0, ""
-            for i in range(len(s)):
-                if s[i] == "+":
-                    op = 1
-                elif s[i] == "-":
-                    cnt = 1
-                    j = i - 1
-                    while j >= 0 and s[j] == "-":
-                        cnt += 1
-                        j -= 1
-                    if cnt & 1 == 1:
-                        op = -1
-                    else:
-                        op = 1          
-                else:
-                    if s[i] != " " and i + 1 < len(s) and not s[i + 1].isdigit():
-                        prev += int((cur + s[i]).strip() ) * op
-                        cur = ""
-                    else:
-                        cur += s[i]
-            if cur.strip().isdigit():
-                return prev + int(cur.strip()) * op
-            return prev
-        stack, cur, i = [], "", 0
-        while i < len(s) or stack:
-            if (i >= len(s) and stack) or s[i] == ")":
-                tmp = []
-                while stack:
-                    ch = stack.pop()
-                    if ch == "(":
-                        break
-                    tmp.append(ch)
-                cur = str(eval("".join(tmp[::-1]) + cur))
-            elif s[i] == "(":
-                stack.append(cur)
-                stack.append("(")
-                cur = ""
-            elif s[i] != " ": cur +=s[i]
-            i += 1
-        return eval(cur)
+        stack, number, sign, result = [], 0, 1, 0
+        for ch in s:
+            if ch.isdigit():
+                number = number * 10 + int(ch)
+            elif ch == "(":
+                stack.append(result)
+                stack.append(sign)
+                result = 0
+                sign = 1
+            elif ch == "+":
+                result += sign * number
+                number = 0
+                sign = 1
+            elif ch == "-":
+                result += sign * number
+                number = 0
+                sign = -1
+            elif ch == ")":
+                result += sign * number
+                result *= stack.pop()
+                result += stack.pop()
+                number = 0
+        if number:
+            result += sign * number
+        return result
