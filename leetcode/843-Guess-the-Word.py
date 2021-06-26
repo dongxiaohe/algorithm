01b6bf02b4c0898ce5c3362411569907b1df2503
Xiaohe Dong <dannyrivclo@gmail.com>
2021-06-25 21:28:52 -0700

    Refactor the solution to use the pivot which can represent most of the words

--- leetcode/843-Guess-the-Word.py
+++ leetcode/843-Guess-the-Word.py
@@ -1,6 +1,15 @@
 class Solution:
     def findSecretWord(self, wordlist, master):
-        for i in range(10):
-            pivot = random.choice(wordlist)
-            number = master.guess(pivot)
-            wordlist = [w for w in wordlist if sum(x == y for x, y in zip(w, pivot)) == number]
+        counter = [None] * 6
+        for i in range(6): counter[i] = Counter(map(lambda x: x[i], wordlist)) 
+        for _ in range(10):
+            score, guess = 0, ""
+            for word in wordlist:
+                tmpScore = 0
+                for i in range(6): tmpScore += counter[i][word[i]]
+                if tmpScore > score:
+                    score = tmpScore
+                    guess = word
+            # print(score, guess, wordlist)
+            number = master.guess(guess)
+            wordlist = [w for w in wordlist if sum([x == y for x, y in zip(guess, w)]) == number]
