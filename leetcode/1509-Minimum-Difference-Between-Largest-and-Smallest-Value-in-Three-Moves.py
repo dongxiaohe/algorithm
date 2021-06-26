9af092f9b3e9363aaa3f3dfb81cabc652fa2b11b
Xiaohe Dong <dannyrivclo@gmail.com>
2021-06-26 12:43:21 -0700

    Modify solution to use the lenear approach.

--- leetcode/1509-Minimum-Difference-Between-Largest-and-Smallest-Value-in-Three-Moves.py
+++ leetcode/1509-Minimum-Difference-Between-Largest-and-Smallest-Value-in-Three-Moves.py
@@ -1,9 +1,13 @@
 class Solution:
     def minDifference(self, nums: List[int]) -> int:
-        if len(nums) <= 4: return 0
-        nums.sort()
-        m = len(nums)
-        diffs = float("inf")
-        for x, y in [(0, 3), (1, 2), (2, 1), (3, 0)]:
-            diffs = min(diffs, abs(nums[x] - nums[m - 1 - y]))
-        return diffs
+        top4 = heapq.nlargest(4, nums)[::-1]
+        bot4 = heapq.nsmallest(4, nums)
+        """
+        1,2,3,4,5,6,7,8,9,10
+        [1, 7],
+        [2, 8],
+        [3, 9],
+        [4, 10]
+        so we inverse top4 or bot4
+        """
+        return min([x - y for x,y in zip(top4, bot4)])
