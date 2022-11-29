1788e9ba950bc8a525e261357b9d70e5ee9e25cf
Xiaohe Dong <dannyrivclo@gmail.com>
2022-11-28 18:59:21 -0800

    refactor the solution 366. Find Leaves of Binary Tree

--- leetcode/366-Find-Leaves-of-Binary-Tree.py
+++ leetcode/366-Find-Leaves-of-Binary-Tree.py
@@ -1,21 +1,13 @@
 class Solution(object):
     def findLeaves(self, root):
-        def _isLeaf(node):
-            return node is not None and node.left is None and node.right is None
         def _dfs(node, res):
-            if node is None: return None
-            if _isLeaf(node.left):
-                res.append(node.left.val)
-                node.left = None
-            if _isLeaf(node.right):
-                res.append(node.right.val)
-                node.right = None
-            _dfs(node.left, res)
-            _dfs(node.right, res)
-        res, arr = [], []
-        while not _isLeaf(root):
-            _dfs(root, arr)
-            res.append(arr)
-            arr = []
-        if root is not None: res.append([root.val])
+            if node is None: return -1
+            level = 1 + max(_dfs(node.left, res), _dfs(node.right, res))
+            if len(res) == level:
+                res.append([])
+            res[level].append(node.val)
+            return level
+        res = []
+        _dfs(root, res)
         return res
+
