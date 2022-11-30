d8d085ea4a306261457c869d8a59b57dc46430e4
Xiaohe Dong <dannyrivclo@gmail.com>
2022-11-29 22:48:04 -0800

    Tweak the solution.

--- leetcode/1710-Maximum-Units-on-a-Truck.py
+++ leetcode/1710-Maximum-Units-on-a-Truck.py
@@ -1,13 +1,13 @@
 class Solution(object):
     def maximumUnits(self, boxTypes, truckSize):
-        total = 0
-        sortedBoxTypes = sorted(boxTypes, key = lambda x: -x[1])
-        i, num = 0, 0
-        while num < truckSize and i < len(sortedBoxTypes):
-            if truckSize - sortedBoxTypes[i][0] - num >= 0:
-                total += sortedBoxTypes[i][1] * sortedBoxTypes[i][0]
-                num += sortedBoxTypes[i][0]
+        total, num = 0, 0
+        hqBoxTypes = list(map(lambda x: (-x[1], x[0]), boxTypes))
+        heapq.heapify(hqBoxTypes)
+        while num < truckSize and hqBoxTypes:
+            size, cnt = heapq.heappop(hqBoxTypes)
+            if truckSize - num - cnt >= 0:
+                total += -size * cnt
+                num += cnt
             else:
-                return total + (truckSize - num) * sortedBoxTypes[i][1]
-            i += 1
+                return total + (truckSize - num) * -size
         return total
